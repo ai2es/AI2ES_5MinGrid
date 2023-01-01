cf8b953944af514c2ac15cecde0685cf000c82a8
William McGovern-Fagg <wrmf@schooner2.oscer.ou.edu>
2022-12-31 18:55:03 -0600

    actually added files lmao

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#SBATCH --job-name=lightning
+#SBATCH --ntasks=1
+#SBATCH --mail-type=ALL
+#SBATCH -p debug
+#SBATCH --mem=28G
+#SBATCH -t 00:20:00
+#SBATCH --mail-user=wrmf@ou.edu
+#SBATCH --output=logs/lightning_%A_stdout.txt
+#SBATCH --error=logs/lightning_%A_sderr.txt
+
+# cd to directory where job was submitted from
+cd $SLURM_SUBMIT_DIR
+pwd
+
+# echo the job id to the slurm file
+# echo $SLURM_ARRAY_TASK_ID
+
+source activate maps
+
+python -u grid.py
