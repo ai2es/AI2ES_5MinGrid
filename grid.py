a9be7ac1449c67261a77ecaf12ecc564fa56d35f
wrmf <31448920+wrmf@users.noreply.github.com>
2023-01-09 12:27:52 -0700

    testing

--- grid.py
+++ grid.py
@@ -8,8 +8,8 @@ import util
 
 extent = [-106, -89, 42.0, 30] #Lat and Long extent of map
 
-filenames = ["TestData.txt", "TestData2.txt"]
-#filenames = ["McGovern1.asc", "McGovern2.asc", "McGovern3.asc", "McGovern4.asc", "McGovern5.asc"]
+#filenames = ["TestData.txt", "TestData2.txt"]
+filenames = ["McGovern1.asc", "McGovern2.asc", "McGovern3.asc", "McGovern4.asc", "McGovern5.asc"]
 columns = ["Date", "Time", "Lat", "Lon", "Magnitude", "Type"]
 
 print("Reading in files...")
@@ -18,6 +18,8 @@ dataframe = pd.DataFrame()
 
 for filename in filenames:
     dataframe = pd.concat([dataframe, pd.read_csv(filename, header = None, delim_whitespace=True, names=columns)], axis=0)
+
+print("Done reading files...")
 
 dataframe['Date'] = dataframe['Date'].str.cat(dataframe['Time'], sep=' ')
 
@@ -30,21 +32,11 @@ dataframe = dataframe.reset_index()
 print(dataframe['Date'])
 
 gridrad = xr.open_dataset('gridrad_grid.nc')
-print(f'{gridrad}\nEEE\n')
-ax,cbar,C = util.boxbin(dataframe['Lon'], dataframe["Lat"], gridrad["Longitude"], gridrad["Latitude"])
-print(ax)
-print(cbar)
-print(C)
-central_lon = np.mean(extent[:2]) # Get central longitude
-central_lat = np.mean(extent[2:]) # Get central latitude
-# ax = plt.axes(projection=ccrs.crs.AlbersEqualArea(central_lon, central_lat)) #Create map
-# ax.set_extent(extent) #Set bounds
-# ax.add_feature(ccrs.feature.OCEAN) #Add ocean to map
-# ax.add_feature(ccrs.feature.LAND, edgecolor='black') #Add land to map
-# ax.add_feature(ccrs.feature.LAKES, edgecolor='black') #Add lakes to map
-# ax.add_feature(ccrs.feature.RIVERS) #Add rivers to map
-# ax.add_feature(ccrs.feature.STATES) #Add states to map
-# ax.gridlines() #Add gridlines
+print(f'Gridrad: {gridrad}')
+ax, cbar, C = util.boxbin(dataframe['Lon'], dataframe["Lat"], gridrad["Longitude"], gridrad["Latitude"])
+print(f"ax: {ax}")
+print(f'cbar: {cbar}')
+print(f'C: {C}')
 
 plt.savefig(f'images/lightningData{0}.png', transparent=False, dpi=1000) #Save figure
  
