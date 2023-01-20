c43982d85339f34e473461605fffe8cb509d4486
wrmf <31448920+wrmf@users.noreply.github.com>
2023-01-20 13:32:13 -0600

    working with images

--- grid.py
+++ grid.py
@@ -1,3 +1,7 @@
+import os
+
+import time
+
 import numpy as np
 import pandas
 import pandas as pd
@@ -10,33 +14,31 @@ import util
 extent = [-106, -89, 42.0, 30] #Lat and Long extent of map
 
 # //ourdisk/hpc/ai2es/hail/nldn/raw/
-filenames = ["TestData.txt", "TestData2.txt", "TestData3.csv"]
-#filenames = ["McGovern1.asc", "McGovern2.asc", "McGovern3.asc", "McGovern4.asc", "McGovern5.asc"]
+#filenames = ["TestData.txt", "TestData2.txt", "TestData3.csv"]
+filenames = ["McGovern1.asc", "McGovern2.asc", "McGovern3.asc", "McGovern4.asc", "McGovern5.asc"]
 columns = ["Date", "Time", "Lat", "Lon", "Magnitude", "Type"]
 
 print("Reading in files...")
 
 dataframe = []
 
+runStart = time.time()
+os.makedirs(f'output/{runStart}')
+
 for filename in filenames:
-    temp = pandas.read_csv(filename,header=None,delim_whitespace=True, names=columns)
+    temp = pandas.read_csv(f'//ourdisk/hpc/ai2es/hail/nldn/raw/{filename}',header=None,delim_whitespace=True, names=columns)
     dataframe.append(temp)
 
 df = pd.concat(dataframe, axis=0, ignore_index=True)
 
-# lon = df[3]
-# lat = df[2]
-
 print("Done reading files...")
 
-print(df)
 df['datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
-print(df)
 df.drop('Date', inplace=True, axis=1)
 df.drop('Time', inplace=True, axis=1)
-startTime = df['datetime'][0].
+startTime = df['datetime'][0].replace(second=0, microsecond=0)
 endTime = startTime + dt.timedelta(0,300)
-#lastTime = df['datetime'][len(df['datetime']-1)]
+lastTime = df['datetime'][len(df)-1].replace(second=0, microsecond=0)
 df = df.set_index('datetime')
 df.sort_values(by='datetime', inplace=True)
 
@@ -44,22 +46,19 @@ df.sort_values(by='datetime', inplace=True)
 xedge = np.arange(-106,-88,0.02083333)
 yedge = np.arange(30,42,0.02083333)
 
-print(startTime)
-#startTime = startTime.replace(hour=0, minute=0, second=0, microsecond=0)
-#print(lastTime)
-print(endTime)
-
 while(endTime <= lastTime):
-    temp = df[slice(startTime.to_string(), endTime.to_string())]
+    temp = df[slice(startTime, endTime)]
     ax, cbar, C = util.boxbin(temp['Lon'], temp['Lat'], xedge, yedge, mincnt=0)
-    if len(C) > 0:
-        plt.savefig(f'images/lightningData{startTime}{endTime}.png', transparent=False, dpi=1000)  # Save figure
+    if len(temp) > 0:
+        plt.savefig(f'output/{runStart}/lightningData{startTime}{endTime}.png', transparent=False, dpi=1000)  # Save figure
+        print(f'Saved lightningData{startTime}{endTime}.png')
         #C.to_netcdf(f'images/lightningData{startTime}{endTime}.png')
     else:
-        continue
+        pass
 
     startTime = endTime
     endTime = endTime + dt.timedelta(0,300)
+    plt.close('all')
 
 print("Done")
 
