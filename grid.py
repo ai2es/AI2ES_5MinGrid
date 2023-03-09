9ab64655faa7022271f02c80893be2f1f81575c7
wrmf <31448920+wrmf@users.noreply.github.com>
2023-03-09 16:55:43 -0600

    fixed code

--- grid.py
+++ grid.py
@@ -26,19 +26,20 @@ os.makedirs(f'output/{runStart}')
 
 xedge = np.arange(-106, -88, 0.02083333)
 yedge = np.arange(30, 42, 0.02083333)
-xmid = np.empty([1, len(xedge)-1])
-ymid = np.empty([1, len(yedge)-1])
+xmid = []
+ymid = []
+
 
 
 i=0
-print(f"34 {len(xedge)}")
-while(i < len(xedge)-2):
-    xmid[i] = (xedge[i]+xedge[i+1])/2
-print("34")
+while(i < len(xedge)-1):
+    xmid.append((xedge[i]+xedge[i+1])/2)
+    i+=1
 i=0
-while(i < len(yedge)-2):
-    ymid[i] = (yedge[i]+yedge[i+1])/2
-print("34")
+while(i < len(yedge)-1):
+    ymid.append((yedge[i]+yedge[i+1])/2)
+    i+=1
+
 
 for filename in filenames:
     df = pandas.read_csv(f'{filename}',header=None,delim_whitespace=True, names=columns)
@@ -53,22 +54,20 @@ for filename in filenames:
     df.sort_values(by='datetime', inplace=True)
 
     beginDate = startTime.replace(hour=0, minute=0, second=0, microsecond=0)
-    #tempArray = xr.DataArray(data_vars=dict(strikes=(["x", "y", "time"], strikes)), coords=dict(lon=(["x", "y"], lon), lat=(["x", "y"], lat), time=time))
 
-    while (endTime <= lastTime):
+    while (startTime <= lastTime):
         temp = df[slice(startTime, endTime)]
         ax, cbar, C = util.boxbin(temp['Lon'], temp['Lat'], xedge, yedge, mincnt=0)
         if len(temp) > 0:
             plt.savefig(f'output/{runStart}/lightningData{startTime}{endTime}.png', transparent=False,
                         dpi=1000)  # Save figure
+
             print(f'Saved lightningData{startTime}{endTime}.png')
 
-            print(C.shape)
-
             tempArray = xr.Dataset(data_vars=dict(strikes=(["x", "y"], C)),
-                                 coords=dict(lon=(["x"], xmid), lat=(["x"], ymid)),attrs=dict(description="Lightning data"),)
+                                 coords=dict(lon=(["x"], xmid), lat=(["y"], ymid)),attrs=dict(description="Lightning data"),)
             tempArray.to_netcdf(path=f'output/{runStart}/lightningData{startTime}.nc')
-            print(f"Saved netcdf {runStart}")
+            print(f"Saved netcdf lightningData{startTime}.nc")
 
         else:
             pass
@@ -79,14 +78,14 @@ for filename in filenames:
 
 print('Finished creating images, now creating gif')
 
-# images = []
-#
-# path = f'output/{runStart}'
-# for filename in sorted(glob.glob(os.path.join(path, '*.png'))):
-#     images.append(imageio.imread(filename))
-#     print(f"Added {filename}")
-#
-# imageio.mimsave(f'output/{runStart}/{runStart}.gif', images, duration=1)
+images = []
+
+path = f'output/{runStart}'
+for filename in sorted(glob.glob(os.path.join(path, '*.png'))):
+    images.append(imageio.imread(filename))
+    print(f"Added {filename}")
+
+imageio.mimsave(f'output/{runStart}/{runStart}.gif', images, duration=1)
 
 print('Done with all')
 
