76560719b315c624f3d1e1f782b456301feac2b0
wrmf <31448920+wrmf@users.noreply.github.com>
2023-03-29 13:49:37 -0500

    5 min stuff?

--- grid.py
+++ grid.py
@@ -8,9 +8,10 @@ import datetime as dt
 import cartopy as ccrs
 import matplotlib.pyplot as plt
 import util
-import imageio.v2 as imageio
+#import imageio.v2 as imageio
 import os
 import glob
+import netCDF4 as nc
 
 extent = [-106, -89, 42.0, 30] #Lat and Long extent of map
 
@@ -48,7 +49,7 @@ for filename in filenames:
     df.drop('Date', inplace=True, axis=1)
     df.drop('Time', inplace=True, axis=1)
     startTime = df['datetime'][0].replace(second=0, microsecond=0)
-    endTime = startTime + dt.timedelta(0, 300)
+    endTime = startTime + dt.timedelta(0, 86400)
     lastTime = df['datetime'][len(df) - 1].replace(second=0, microsecond=0)
     df = df.set_index('datetime')
     df.sort_values(by='datetime', inplace=True)
@@ -56,43 +57,30 @@ for filename in filenames:
     beginDate = startTime.replace(hour=0, minute=0, second=0, microsecond=0)
 
     while (startTime <= lastTime):
-        temp = df[slice(startTime, endTime)]
-        ax, cbar, C = util.boxbin(temp['Lon'], temp['Lat'], xedge, yedge, mincnt=0)
-        if len(temp) > 0:
-            plt.savefig(f'output/{runStart}/lightningData{startTime}{endTime}.png', transparent=False,
-                        dpi=1000)  # Save figure
+        tempArray = xr.Dataset()
+        currentTime = startTime
+        while currentTime <= endTime:
+            temp = df[slice(currentTime, currentTime+dt.timedelta(0, 300))]
+            ax, cbar, C = util.boxbin(temp['Lon'], temp['Lat'], xedge, yedge, mincnt=0)
+            if len(temp) > 0:
 
-            print(f'Saved lightningData{startTime}{endTime}.png')
+                if currentTime == startTime:
+                    tempArray = xr.Dataset(data_vars=dict(strikes=(["x", "y"], C)),
+                                         coords=dict(lon=(["x"], xmid), lat=(["y"], ymid),
+                                                     time=(['time'], np.arange(startTime, endTime, 300000000))),
+                                           attrs=dict(description="Lightning data"),)
+                else:
+                    tempArray2 = xr.Dataset(data_vars=dict(strikes=(["x", "y"], C)),
+                                           coords=dict(lon=(["x"], xmid), lat=(["y"], ymid),
+                                                       time=(['time'], np.arange(startTime, endTime, 300000000))),
+                                       attrs=dict(description="Lightning data"), )
+                    tempArray = xr.concat([tempArray, tempArray2], data_vars='all', dim='i')
+            else:
+                pass
 
-            tempArray = xr.Dataset(data_vars=dict(strikes=(["x", "y"], C)),
-                                 coords=dict(lon=(["x"], xmid), lat=(["y"], ymid)),attrs=dict(description="Lightning data"),)
-            tempArray.to_netcdf(path=f'output/{runStart}/lightningData{startTime}.nc')
-            print(f"Saved netcdf lightningData{startTime}.nc")
+            currentTime = currentTime+dt.timedelta(0, 300)
 
-        else:
-            pass
-
+        tempArray.to_netcdf(path=f'output/{runStart}/lightningData{str(startTime).split(" ")[0]}.nc')
+        print(f"Saved netcdf lightningData{str(startTime).split(' ')[0]}.nc")
         startTime = endTime
-        endTime = endTime + dt.timedelta(0, 300)
-        plt.close('all')
-
-print('Finished creating images, now creating gif')
-
-images = []
-
-path = f'output/{runStart}'
-for filename in sorted(glob.glob(os.path.join(path, '*.png'))):
-    images.append(imageio.imread(filename))
-    print(f"Added {filename}")
-
-imageio.mimsave(f'output/{runStart}/{runStart}.gif', images, duration=1)
-
-print('Done with all')
-
-
-
-#set index to datetime, slice on that
-#save netcdf, one for each 5 min lat, lon grid and time in file
-#C matrix may be transposed
-#
-#
+        endTime = endTime + dt.timedelta(0, 86400)
