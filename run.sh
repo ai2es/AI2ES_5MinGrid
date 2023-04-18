8886312dc22538776f138ff690a21654424e37fc
wrmf <31448920+wrmf@users.noreply.github.com>
2023-04-17 22:15:54 -0500

    fixed run.sh file

--- run.sh
+++ run.sh
@@ -5,11 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -p normal
 #SBATCH --mem=28G
-<<<<<<< HEAD
 #SBATCH -t 12:00:00
-=======
-#SBATCH -t 00:30:00
->>>>>>> 44484fee9563840a465c0080309888fbbf906562
 #SBATCH --mail-user=wrmf@ou.edu
 #SBATCH --output=logs/lightning_%A_stdout.txt
 #SBATCH --error=logs/lightning_%A_sderr.txt
